55197a819e2fc395b160b7274066715e6c4fa4db
jn19392 <60779279+jn19392@users.noreply.github.com>
2022-02-03 15:32:56 +0000

    Started cleaning up data

--- load_data.py
+++ load_data.py
@@ -4,13 +4,23 @@ import numpy as numpy
 def load_data():
     EU_em_df = pd.read_excel('Data/Table_11.5_Carbon_Dioxide_Emissions_From_Energy_Consumption__Transportation_Sector.xlsx')
     EU_em_annual_df = pd.read_csv('Data/EU_annual_carbon_emission_by_road_transportation.csv')
-    UK_em_df = pd.read_excel('Data/uk-env0201.ods')
+    UK_em_df = pd.read_excel('Data/uk-env0201.ods', nrows = 16)
     US_em_df = pd.read_excel('Data/US_em.xlsx')
     EU_veh_df = pd.read_csv('Data/new-electric-vehicles-in-eu-1.csv')
     UK_veh_df = pd.read_excel('Data/veh0203.ods', nrows= 76)
     US_veh_df = pd.read_excel('Data/10354_epact_vehicle_history_5-21-21.xlsx')
     
+    EU_em_df = EU_em_df.set_index('Month')
+    UK_em_df = UK_em_df.transpose()
+    
     return EU_em_df, EU_em_annual_df, UK_em_df, US_em_df, EU_veh_df,UK_veh_df, US_veh_df
 
 EU_em_df, EU_em_annual_df, UK_em_df, US_em_df, EU_veh_df,UK_veh_df, US_veh_df = load_data()
 
+
+
+
+
+print(US_em_df)
+
+#EU_em_df, UK_em_df, US_em_df
