63d9eb1ec2e7a0db7418c9f151c4afe43ae40f63
jn19392 <60779279+jn19392@users.noreply.github.com>
2022-03-02 11:18:27 +0000

    created pred file

--- load_data.py
+++ load_data.py
@@ -1,26 +1,50 @@
-import pandas as pd 
-import numpy as numpy
+import numpy as np
+import pandas as pd
 
-def load_data():
-    EU_em_df = pd.read_excel('Data/Table_11.5_Carbon_Dioxide_Emissions_From_Energy_Consumption__Transportation_Sector.xlsx')
-    EU_em_annual_df = pd.read_csv('Data/EU_annual_carbon_emission_by_road_transportation.csv')
-    UK_em_df = pd.read_excel('Data/uk-env0201.ods', nrows = 16)
-    US_em_df = pd.read_excel('Data/US_em.xlsx')
-    EU_veh_df = pd.read_csv('Data/new-electric-vehicles-in-eu-1.csv')
-    UK_veh_df = pd.read_excel('Data/veh0203.ods', nrows= 76)
-    US_veh_df = pd.read_excel('Data/10354_epact_vehicle_history_5-21-21.xlsx')
+def load_data(em_data = True, veh_data = True, air_data = True):
+    args = []
+    if em_data:
+        EU_em_df = pd.read_excel('Data/EU_em.xlsx',engine='openpyxl')
+        EU_em_annual_df = pd.read_csv('Data/EU_em_annual.csv',)
+        UK_em_df = pd.read_excel('Data/UK_em.ods', nrows = 16)
+        US_em_df = pd.read_excel('Data/US_em.xlsx',engine='openpyxl')
+        # Emissions data Processing
+        EU_em_df = EU_em_df.set_index('Month')
+        EU_em_annual_df = EU_em_df.groupby(np.arange(len(EU_em_df))//(12)).sum()
+        EU_em_annual_df.index = np.linspace(1973, 2021, EU_em_annual_df.shape[0], dtype=np.int32)
+        UK_em_df = UK_em_df.transpose()
+        UK_em_df.columns = UK_em_df.iloc[0, :]
+        UK_em_df = UK_em_df.drop('Year')
+        US_em_df = US_em_df.transpose()
+        US_em_df.columns = US_em_df.iloc[0, :]
+        US_em_df = US_em_df.drop('Gas/Vehicle')
+        args += [EU_em_df, EU_em_annual_df, UK_em_df, US_em_df]
+    if veh_data:
+        EU_veh_df = pd.read_csv('Data/EU_veh.csv')
+        UK_veh_df = pd.read_excel('Data/UK_veh.ods', nrows= 76)
+        US_veh_df = pd.read_excel('Data/US_veh.xlsx')
+        args += [EU_veh_df, UK_veh_df, US_veh_df]
+    if air_data:
+        # Secondary Data 
+        # Air pollution
+        # UK 1970-2019/2022
+        UK_nox_annual_df = pd.read_csv('Data/Figure06_NOx_time_series.csv')
+        UK_pm_all_annual_df = pd.read_csv('Data/Figure03_PM_time_series.csv')
+                
+        # USA Since 1970-2020, plz Access through column id(numbers)
+        USA_nox_annual_df = pd.read_csv('Data/US_nox_em_time_series.csv') # National Emissions Totals (thousands of tons)
+        USA_pm_10_annual_df = pd.read_csv('Data/US_pm10_year.csv')        #
+        USA_pm_2_5_annual_df = pd.read_csv('Data/US_pm2_5_year.csv')
+        
+        USA_pm_2_5_annual_df = USA_pm_2_5_annual_df.transpose()
+        USA_pm_10_annual_df = USA_pm_10_annual_df.transpose()
+                
+        # OCED DATA
+        OCED_PM10_df = pd.read_excel('Data/PM10_ROAD_OCED_WORLD_DATA.xlsx')
+        OCED_NOX_df = pd.read_excel('Data/NOX_ROAD_OCED_WORLD_DATA.xlsx')
+        OCED_PM2_5_df = pd.read_excel('Data/PM2_5_ROAD_OCED_WORLD_DATA.xlsx')
     
-    EU_em_df = EU_em_df.set_index('Month')
-    UK_em_df = UK_em_df.transpose()
+    args += [UK_nox_annual_df,UK_pm_all_annual_df,USA_pm_10_annual_df,USA_pm_2_5_annual_df,USA_nox_annual_df,OCED_PM10_df,OCED_NOX_df,OCED_PM2_5_df]
     
-    return EU_em_df, EU_em_annual_df, UK_em_df, US_em_df, EU_veh_df,UK_veh_df, US_veh_df
+    return args
 
-EU_em_df, EU_em_annual_df, UK_em_df, US_em_df, EU_veh_df,UK_veh_df, US_veh_df = load_data()
-
-
-
-
-
-print(US_em_df)
-
-#EU_em_df, UK_em_df, US_em_df
